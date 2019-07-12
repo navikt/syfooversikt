cf5e16d1c1a1ea1bd7380e117aa7414c6eb1c73c
kristianstorvoll <kristianstorvoll@gmail.com>
2019-07-12 13:43:05 +0200

    --

--- src/components/TekstFilter.tsx
+++ src/components/TekstFilter.tsx
@@ -4,19 +4,17 @@ import { Panel } from 'nav-frontend-paneler';
 import classNames from 'classnames';
 
 interface Props extends React.PropsWithoutRef<any> {
-    // todo
     initialValue?: string;
     onFilterChange: (query: string) => void;
 }
 
 export default ({ onFilterChange, initialValue, className }: Props) => {
 
-    const [value, setValue] = useState(initialValue || '');
+    const [ value, setValue ] = useState(initialValue || '');
 
     return (
         <Panel className={classNames(className, 'TekstFilter')} >
-            <Input label={'Navn / Fødselsnummer'} placeholder={'Søk på navn eller fødselsnummer'}  value={value} onChange={(e) => {
-                // tslint:disable-next-line:no-console
+            <Input label={'Navn / Fødselsnummer'} placeholder={'Søk på navn eller fødselsnummer'} value={value} onChange={(e) => {
                 setValue(e.target.value);
                 onFilterChange(e.target.value);
             }} />
