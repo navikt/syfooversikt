63215868627efd5e2e64506f73757121b440ff8f
Erik Jansen <erik.gunnar.jansen@nav.no>
2021-08-25 10:13:48 +0200

    Delete unused functions

--- src/utils/miljoUtil.ts
+++ src/utils/miljoUtil.ts
@@ -1,17 +1,9 @@
-export const finnMiljoStreng = (): string => {
-  return erPreProd() ? '-q1' : '';
-};
-
 export const erPreProd = (): boolean => {
   return window.location.href.indexOf('dev.intern.nav.no') > -1;
 };
 
 export const erLokal = (): boolean => {
   return window.location.host.indexOf('localhost') > -1;
-};
-
-export const erHerokuApp = (): boolean => {
-  return window.location.href.indexOf('herokuapp') > -1;
 };
 
 export const finnNaisUrlDefault = (): string => {
@@ -23,29 +15,4 @@ export const fullNaisUrlDefault = (host: string, path: string): string => {
     return `http://localhost:8081${path}`;
   }
   return `https://${host}${finnNaisUrlDefault()}${path}`;
-};
-
-export const finnNaisUrlQ1 = (): string => {
-  return erPreProd() ? '-q1.nais.preprod.local' : '.nais.adeo.no';
-};
-
-export const fullNaisUrlQ1 = (host: string, path: string): string => {
-  if (erLokal()) {
-    return path;
-  }
-  return `https://${host}${finnNaisUrlQ1()}${path}`;
-};
-
-export const getKubernetesServiceUrl = (host: string, path: string): string => {
-  if (erLokal()) {
-    return path;
-  }
-  return `http://${host}${path}`;
-};
-
-export const fullAppAdeoUrl = (path: string): string => {
-  if (erLokal()) {
-    return path;
-  }
-  return `https://app${finnMiljoStreng()}.adeo.no${path}`;
 };
