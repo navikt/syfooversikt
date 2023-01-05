3ea07e5e28aacf6c679d0c517ea087a9f13291bf
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2023-01-05 15:07:18 +0100

    Format aktivitetskravUpdatedAt in AVVENT label (#295)

--- test/utils/dateUtils.test.ts
+++ test/utils/dateUtils.test.ts
@@ -1,5 +1,5 @@
 import { expect } from 'chai';
-import { getWeeksBetween } from '@/utils/dateUtils';
+import { getWeeksBetween, toReadableDate } from '@/utils/dateUtils';
 
 describe('dateUtils', () => {
   describe('week calculations', () => {
@@ -7,6 +7,22 @@ describe('dateUtils', () => {
       const date1 = new Date('2022-10-01');
       const date2 = new Date('2022-10-14');
       expect(getWeeksBetween(date1, date2)).to.equal(2);
+    });
+  });
+  describe('readable date', () => {
+    it('returns empty string if date is null', () => {
+      const readableDate = toReadableDate(null);
+      expect(readableDate).to.equal('');
+    });
+    it('returns string DD.MM.YYYY from date', () => {
+      const date = new Date('2020-02-03');
+      const readableDate = toReadableDate(date);
+      expect(readableDate).to.equal('03.02.2020');
+    });
+    it('returns string DD.MM.YYYY from date with time', () => {
+      const date = new Date('2020-12-01T10:12:05.913826');
+      const readableDate = toReadableDate(date);
+      expect(readableDate).to.equal('01.12.2020');
     });
   });
 });
