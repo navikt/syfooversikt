18f15a101cb01c4d8421b083e06386c03aa3281c
Anders Rognstad <79838644+andersrognstad@users.noreply.github.com>
2024-03-15 11:44:57 +0100

    Litt Aksel-migrering (#413)
    * Migrate TekstFilter
    
    * Migrate EmptyDrawer
    
    * Migrate lenke

--- src/components/EmptyDrawer/EmptyDrawer.tsx
+++ src/components/EmptyDrawer/EmptyDrawer.tsx
@@ -1,19 +1,17 @@
 import { EmptyDrawerImage } from '../../../img/ImageComponents';
 import React from 'react';
-import { Ingress } from 'nav-frontend-typografi';
+import { BodyShort } from '@navikt/ds-react';
 
 export const texts = {
   ingenHendelser: 'Vi finner ingen personer som har hendelser',
   altText: 'Her var det ikke mye',
 };
 
-export const EmptyDrawer = () => {
-  return (
-    <div className="mt-16">
-      <div className="text-center">
-        <img alt={texts.altText} src={EmptyDrawerImage} />
-        <Ingress className="mt-8">{texts.ingenHendelser}</Ingress>
-      </div>
-    </div>
-  );
-};
+export const EmptyDrawer = () => (
+  <div className="mt-16 text-center">
+    <img alt={texts.altText} src={EmptyDrawerImage} />
+    <BodyShort size="large" className="mt-8">
+      {texts.ingenHendelser}
+    </BodyShort>
+  </div>
+);
