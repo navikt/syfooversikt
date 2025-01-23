36ac2f3b660afbba1896bdf62ff28cf723c0dcdb
Ingrid Gladheim <ingridgladheim@hotmail.com>
2025-01-23 15:40:25 +0100

    IS-3030: fix uu nullstill button

--- src/sider/oversikt/filter/ClearFiltersButton.tsx
+++ src/sider/oversikt/filter/ClearFiltersButton.tsx
@@ -1,52 +1,28 @@
 import React, { ReactElement } from 'react';
-import styled from 'styled-components';
-import themes from '@/styles/themes';
-import SlettIkon from '@/sider/oversikt/filter/SlettIkon';
 import { useFilters } from '@/context/filters/FilterContext';
 import { ActionType } from '@/context/filters/filterContextActions';
+import { Button } from '@navikt/ds-react';
+import { TrashIcon } from '@navikt/aksel-icons';
 
 const texts = {
   nullstill: 'Nullstill valg',
 };
 
-const ClearFiltersWrapper = styled.div`
-  display: inline-flex;
-  align-items: center;
-  box-sizing: border-box;
-  justify-content: flex-end;
-  color: ${themes.color.navBla};
-  font-size: 1em;
-  font-weight: 500;
-  padding: 0.5rem 0.25rem;
-  -webkit-user-select: none;
-  user-select: none;
-
-  :hover {
-    cursor: pointer;
-  }
-
-  :active {
-    border: 2px solid ${themes.color.navBla};
-  }
-
-  > svg {
-    margin-right: 0.25em;
-  }
-`;
-
 export const ClearFiltersButton = (): ReactElement => {
   const { dispatch: dispatchFilterAction } = useFilters();
 
   return (
-    <ClearFiltersWrapper
+    <Button
+      variant="tertiary"
+      size="small"
+      icon={<TrashIcon title="a11y-title" fontSize="1.5rem" />}
       onClick={() => {
         dispatchFilterAction({
           type: ActionType.ResetFilters,
         });
       }}
     >
-      <SlettIkon fargeKode={themes.color.navBla} />
       {texts.nullstill}
-    </ClearFiltersWrapper>
+    </Button>
   );
 };
