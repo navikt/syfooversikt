1e46f21bccc53c29e337ca3d1586032b92764996
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-02-25 10:25:49 +0100

    IS-3097: Endret slik at elementer i menyen ser like ut (#604)

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,14 +1,16 @@
 import React, { ReactElement } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
-import { MoteoversiktLink } from '@/components/MoteoversiktLink';
 import { Box, Heading, HStack, Tabs } from '@navikt/ds-react';
 import { MagnifyingGlassIcon } from '@navikt/aksel-icons';
 import { routes } from '@/routers/routes';
+import { linkToNewHostAndPath, Subdomain } from '@/utils/miljoUtil';
+import LinkAsTab from '@/components/LinkAsTab';
 
 const texts = {
   enhetensOversikt: 'Enhetens oversikt',
   minOversikt: 'Min oversikt',
   sokSykmeldt: 'Søk etter sykmeldt',
+  moteoversikt: 'Mine møter',
 };
 
 export const NavigationBar = (): ReactElement => {
@@ -17,27 +19,31 @@ export const NavigationBar = (): ReactElement => {
 
   return (
     <Box className="mb-4" background="surface-default">
-      <HStack className="w-11/12 m-auto">
+      <HStack className="w-11/12">
         <Tabs value={pathname} onChange={(value) => navigate(value)}>
           <Tabs.List>
             <Tabs.Tab
               value={routes.MIN_OVERSIKT}
-              label={<Heading size="small">{texts.minOversikt}</Heading>}
+              label={<Heading size="xsmall">{texts.minOversikt}</Heading>}
             />
             <Tabs.Tab
               value={routes.ENHET_OVERSIKT}
-              label={<Heading size="small">{texts.enhetensOversikt}</Heading>}
+              label={<Heading size="xsmall">{texts.enhetensOversikt}</Heading>}
+            />
+            <LinkAsTab
+              href={linkToNewHostAndPath(
+                Subdomain.SYFOMOTEOVERSIKT,
+                '/syfomoteoversikt/minemoter'
+              )}
+              label={<Heading size="xsmall">{texts.moteoversikt}</Heading>}
             />
             <Tabs.Tab
               value={routes.SOK_SYKMELDT}
               icon={<MagnifyingGlassIcon />}
-              label={<Heading size="small">{texts.sokSykmeldt}</Heading>}
+              label={<Heading size="xsmall">{texts.sokSykmeldt}</Heading>}
             />
           </Tabs.List>
         </Tabs>
-        <div className="ml-auto self-center">
-          <MoteoversiktLink />
-        </div>
       </HStack>
     </Box>
   );
