e5c0ba1146ce45cf5f2d1bafb268b08fe092b1ab
Marius Syverstad Fossum <marius.syverstad.fossum@nav.no>
2025-03-18 07:41:00 +0100

    IS-3179: Lagt til informasjon om forhåndsvarsler som ikke har blitt sendt ut

---
+++ src/components/Systemvarsel.tsx
@@ -0,0 +1,16 @@
+import { Alert, BodyShort } from '@navikt/ds-react';
+import React from 'react';
+
+const texts = {
+  forhandsvarselSystemfeil:
+    'Grunnet teknisk feil har ikke forhåndsvarsel i perioden 27. februar – 12. mars blitt varslet på riktig måte. I mange av sakene har bruker fått forlenget frist, og fristen i Modia er oppdatert. Dersom berørte brukere tar kontakt, skal det gis forlenget frist.',
+};
+
+//TODO: Kan fjernes etter at ny frist har utgått: 09.04.25
+export default function Systemvarsel() {
+  return (
+    <Alert variant={'error'} size={'medium'} className={'mb-4'}>
+      <BodyShort>{texts.forhandsvarselSystemfeil}</BodyShort>
+    </Alert>
+  );
+}
