412826533865ac15af6c1220447d5218aa2c44d8
Solgård <vetle.bu.solgard@nav.no>
2025-03-31 14:26:49 +0200

    IS-3146: Add feature toggle for tildeling functionality

--- src/data/unleash/types/unleash_types.ts
+++ src/data/unleash/types/unleash_types.ts
@@ -5,8 +5,10 @@ export type Toggles = {
 // See toggles: https://teamsykefravr-unleash-web.nav.cloud.nais.io/features
 export enum ToggleNames {
   isFlexjarArenaEnabled = 'isFlexjarArenaEnabled',
+  isTildelOppfolgingsenhetEnabled = 'isTildelOppfolgingsenhetEnabled',
 }
 
 export const defaultToggles: Toggles = {
   isFlexjarArenaEnabled: false,
+  isTildelOppfolgingsenhetEnabled: false,
 };
